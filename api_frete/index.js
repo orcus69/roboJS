36648ad86ee42c28b75153730f2ecc4c3f9634e4
orcus69 <josusantos2@gmail.com>
2023-02-02 09:07:52 -0300

    headless true

--- api_frete/index.js
+++ api_frete/index.js
@@ -7,7 +7,7 @@ app.get('/frete', async (req, res) => {
   const { dest } = req.query;
 
   try {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     await page.goto('https://melhorenvio.com.br/');
     console.log('Carregando...');
@@ -19,23 +19,23 @@ app.get('/frete', async (req, res) => {
     await page.waitForSelector('[class="calculator-cards-mobile"]');
 
     const data = await page.evaluate(_ =>
-        Array.from(
-            document.querySelectorAll("#calculator > div > div.calculator-table-container > div.calculator-cards-mobile > div > div.shipping-text-info > div.shipping-price-info > div > p.final-price.final-text")
+      Array.from(
+        document.querySelectorAll("#calculator > div > div.calculator-table-container > div.calculator-cards-mobile > div > div.shipping-text-info > div.shipping-price-info > div > p.final-price.final-text")
 
-        ).map(e => {
-            return {
-                empresa: e.parentNode.parentNode.parentNode.parentNode.childNodes[0].children[0].children[0].getAttribute('alt'),
-                modalidade: e.parentNode.parentNode.parentNode.firstElementChild.children[1].innerText,
-                prazo: e.parentNode.parentNode.parentNode.children[1].children[1].innerText,
-                prices: e.innerText
-            }
-        })
+      ).map(e => {
+        return {
+          empresa: e.parentNode.parentNode.parentNode.parentNode.childNodes[0].children[0].children[0].getAttribute('alt'),
+          modalidade: e.parentNode.parentNode.parentNode.firstElementChild.children[1].innerText,
+          prazo: e.parentNode.parentNode.parentNode.children[1].children[1].innerText,
+          prices: e.innerText
+        }
+      })
     );
-    
+
     browser.close();
 
     return res.json({ data });
-    
+
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
